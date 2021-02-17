5981382fde31104ca2eabcc4b313d9163496d955
signalarun <signalarun@gmail.com>
2021-02-17 17:06:36 +0530

    update

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const Hapi = require('hapi');
+const Hapi = require('@hapi/hapi');
+const Path = require('path');
 
 console.log('Hello world we are initializing..');
 
 // init server
 const server = new Hapi.Server({
     port: 8080,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'public')
+        }
+    }
 });
 
-
 // defining routes
-
 server.route({
     method : 'get',
     path : '/',
@@ -19,17 +23,26 @@ server.route({
     }
 });
 
+server.route({
+    method: 'GET',
+    path: '/picture.jpg',
+    handler: function (request, h) {
+
+        return h.file('picture.jpg');
+    }
+});
 
 // Starting server
 async function start() {
     // start your server
     try {
+        await server.register(require('@hapi/inert'));
         await server.start()
     } catch (err) {
         console.error(err)
         process.exit(1)
     }
-    console.log('Server running at: ', server.info.uri)
+    console.log('Server running at : ', server.info.uri)
 }
 
 start()
